1bf5af80029960a30524833d8a69334c000ef782
Adriano Di Piero <adrianodipiero@gmail.com>
2020-03-23 13:22:47 -0300

    Update colonization.php

--- colonization.php
+++ colonization.php
@@ -3,7 +3,7 @@
  * Plugin Name: Colonization
  * Plugin URI: https://github.com/dricdolphin/colonization
  * Description: Plugin de WordPress com o sistema de jogo de Colonization.
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: dricdolphin
  * Author URI: https://dricdolphin.com
  */
